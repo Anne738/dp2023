34957b73192966b51e68013111ca9fa371ebb0f4
Anne738 <94481296+Anne738@users.noreply.github.com>
2023-01-31 11:45:35 +0200

    +

---
+++ lab1b/src/main/java/Servlets/ServletOne.java
@@ -0,0 +1,28 @@
+package Servlets;
+
+import Elements.Element;
+import com.google.gson.Gson;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/servlet")
+public class ServletOne extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Element entity = new Element("JAVELIN", 178000 );
+
+        String someJson = new Gson().toJson(entity);
+
+        PrintWriter out = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        out.print(someJson);
+        out.flush();
+    }
+}
