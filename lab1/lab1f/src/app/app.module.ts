24e60497dbab5ffbc57904ca66414d8c38529c2d
Anne738 <94481296+Anne738@users.noreply.github.com>
2023-02-01 20:50:25 +0200

    changing styles

--- lab1/lab1f/src/app/app.module.ts
+++ lab1/lab1f/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ElementsComponent } from './elements/elements.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ElementsComponent
+    ElementsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
