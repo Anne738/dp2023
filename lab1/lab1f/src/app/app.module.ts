32fa578bb39ef512e96494b3eef9a2fc23525bd8
Anne738 <94481296+Anne738@users.noreply.github.com>
2023-02-02 21:07:12 +0200

    add component home

--- lab1/lab1f/src/app/app.module.ts
+++ lab1/lab1f/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ElementsComponent } from './elements/elements.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ElementsComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
