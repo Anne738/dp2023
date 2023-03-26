f133ba863ed18d3e67411134ee663e5f7f00a79a
Anne738 <94481296+Anne738@users.noreply.github.com>
2023-03-27 02:06:50 +0300

    +

---
+++ lab5/lab5/src/main/java/knu/fit/mit/lab5/AppController.java
@@ -0,0 +1,47 @@
+package knu.fit.mit.lab5;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class AppController {
+    @Autowired
+    private ElementService service;
+
+    @GetMapping("/")
+    public String viewHomePage(Model model){
+        List<Element> listElement = service.listAll();
+        model.addAttribute("listElement",listElement);
+        return "index";
+    }
+    @GetMapping("/new")
+    public String AddElem(Model model){
+        Element element = new Element();
+        model.addAttribute("element", element);
+        return "new_elem";
+    }
+    @PostMapping("/new")
+    public String saveElem(@ModelAttribute("element") Element element) {
+        service.save(element);
+        return "redirect:/";
+    }
+    @GetMapping("/update/{id}")
+    public ModelAndView UpdateForm(@PathVariable(name = "id") Long id){
+        ModelAndView mav = new ModelAndView("updated_element");
+        Element element = service.get(id);
+        mav.addObject("element", element);
+
+        return mav;
+    }
+
+    @GetMapping("/delete/{id}")
+    public String DeleteElem(@PathVariable(name = "id") Long id){
+        service.delete(id);
+        return "redirect:/";
+    }
+}
---
+++ lab5/lab5/src/main/java/knu/fit/mit/lab5/ElementRepository.java
@@ -0,0 +1,7 @@
+package knu.fit.mit.lab5;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ElementRepository extends JpaRepository<Element, Long> {
+
+}
